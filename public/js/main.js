7a260500f0cdfd81ac4e53d0f0152c5e101c7569
kaluchii <mr.kaluchii@mail.ru>
2017-09-28 08:55:19 +0600

    Test

--- public/js/main.js
+++ public/js/main.js
@@ -293,8 +293,5 @@ $(document).ready(function () {
         $('.mobile-detail__item').removeClass('is-open');
     });
 
-    $('.about__text-col, .about__tagline, .contacts__contact-block, .feedbacks__wrapper').viewportChecker({
-        classToAdd: 'display',
-        offset: '40%'
-    });
+
 });
--- resources/views/front/index/index.blade.php
+++ resources/views/front/index/index.blade.php
@@ -336,5 +336,14 @@
             </div>
         </div>
     </section>
+
+    <script>
+        $(window).on('load', function () {
+            $('.about__text-col, .about__tagline, .contacts__contact-block, .feedbacks__wrapper').viewportChecker({
+                classToAdd: 'display',
+                offset: '40%'
+            });
+        });
+    </script>
     @include('front.index.meta')
 @endsection
