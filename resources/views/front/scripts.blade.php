5212ce9cecba19253b5e70c31e4047f6055ca3fc
kaluchii <mr.kaluchii@mail.ru>
2017-09-28 10:29:09 +0600

    Test

--- resources/views/front/scripts.blade.php
+++ resources/views/front/scripts.blade.php
@@ -1,10 +1,10 @@
 @section('scripts')
-    <script  src="/js/jquery.min.js"></script>
-    <script  src="/js/jquery.magnific-popup.min.js"></script>
-    <script  src="/js/jquery.mask.js"></script>
-    <script  src="/js/form.js"></script>
-    <script  src="/js/fotorama.js"></script>
-    <script  src="/js/jquery.viewportchecker.min.js"></script>
-    <script  src="https://api-maps.yandex.ru/2.1/?lang=ru_RU"></script>
-    <script  src="/js/main.js"></script>
+    <script defer src="/js/jquery.min.js"></script>
+    <script defer src="/js/jquery.magnific-popup.min.js"></script>
+    <script defer src="/js/jquery.mask.js"></script>
+    <script defer src="/js/form.js"></script>
+    <script defer src="/js/fotorama.js"></script>
+    <script defer src="/js/jquery.viewportchecker.min.js"></script>
+    <script defer src="https://api-maps.yandex.ru/2.1/?lang=ru_RU"></script>
+    <script defer src="/js/main.js"></script>
 @endsection
